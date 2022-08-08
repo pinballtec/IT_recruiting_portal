f89ff8a93a06907c6420f28dd4e32bd56d5c1f89
Daniil_Boika <daniilboiko99@gmail.com>
2022-08-08 20:06:53 +0200

    added simple showcase + models configuration + admin

--- it_reqruting/app_projects/views.py
+++ it_reqruting/app_projects/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from .models import Project
 # Create your views here.
+
+
+def projects(request):
+    projects = Project.objects.all()
+    context = {'projects': projects}
+    return render(request, 'app_projects/projects.html', context)
+
+
+def project(request, pk):
+    project_single = Project.objects.get(id=pk)
+    tags = project_single.tags.all()
+    return render(request, 'app_projects/single-project.html', {'project': project_single})
